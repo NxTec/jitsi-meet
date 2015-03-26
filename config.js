64a16ed3d30476d2ac5516f51f29c0e3b4cd7d9e
Sean Campbell <sean@seans-mbp.gateway.pace.com>
2015-03-26 15:25:15 -0700

    Update config.js to reflect what is on server, plus updated the chrome extension id

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 var config = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'webrtcdev.collaborate.center',
         //anonymousdomain: 'guest.example.com',
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
-        //jirecon: 'jirecon.jitsi-meet.example.com',
-        //call_control: 'callcontrol.jitsi-meet.example.com',
-        //focus: 'focus.jitsi-meet.example.com' - defaults to 'focus.jitsi-meet.example.com'
+        muc: 'conference.webrtcdev.collaborate.center', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.webrtcdev.collaborate.center', // FIXME: use XEP-0030
+        //jirecon: 'jirecon.webrtcdev.collaborate.center',
+        //call_control: 'callcontrol.webrtcdev.collaborate.center',
+        //focus: 'focus.webrtcdev.collaborate.center' - defaults to 'focus.webrtcdev.collaborate.center'
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//webrtcdev.collaborate.center/http-bind', // FIXME: use xep-0156 for that
     clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
-    //focusUserJid: 'focus@auth.jitsi-meet.example.com', // The real JID of focus participant - can be overridden here
+    //focusUserJid: 'focus@auth.webrtcdev.collaborate.center', // The real JID of focus participant - can be overridden here
     //defaultSipNumber: '', // Default SIP number
     desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
-    chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
+    chromeExtensionId: 'adegdgcakdkmpacfffmjbjhagnkoncpj', // Id of desktop streamer Chrome extension
     desktopSharingSources: ['screen', 'window'],
     minChromeExtVersion: '0.1', // Required version of Chrome extension
     openSctp: true, // Toggle to enable/disable SCTP channels
